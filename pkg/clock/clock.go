167914812783228e205dbf363d31e6b12def0454
FishGoddess <1149062639@qq.com>
2023-02-05 23:20:24 +0800

    完善 clock

--- pkg/clock/clock.go
+++ pkg/clock/clock.go
@@ -28,13 +28,22 @@ const (
 // In fact, we don't recommend you to use it unless you have to...
 // According to our benchmarks, it does run faster than time.Now:
 //
-// $ go test -bench=. -benchtime=1s ./clock.go ./clock_test.go
+// In my win10 pc:
 // goos: windows
 // goarch: amd64
 // cpu: AMD Ryzen 7 5800X 8-Core Processor
 // BenchmarkTimeNow-16             338466458                3.523 ns/op           0 B/op          0 allocs/op
 // BenchmarkClockNow-16            1000000000               0.2165 ns/op          0 B/op          0 allocs/op
 //
+// In my macbook with charging:
+// goos: darwin
+// goarch: amd64
+// pkg: github.com/FishGoddess/cachego/pkg/clock
+// cpu: Intel(R) Core(TM) i7-9750H CPU @ 2.60GHz
+// BenchmarkTimeNow-12             17841402                67.05 ns/op            0 B/op          0 allocs/op
+// BenchmarkClockNow-12            1000000000               0.2528 ns/op          0 B/op          0 allocs/op
+//
+// In my cloud server using 2 cores and running benchmarks in docker container:
 // goos: linux
 // goarch: amd64
 // pkg: github.com/FishGoddess/cachego/pkg/clock
@@ -42,8 +51,12 @@ const (
 // BenchmarkTimeNow-2      17946441                65.62 ns/op            0 B/op          0 allocs/op
 // BenchmarkClockNow-2     1000000000               0.3162 ns/op          0 B/op          0 allocs/op
 //
+// PS: All benchmarks are ran with "go test -bench=. -benchtime=1s".
+//
 // However, the performance of time.Now is faster enough in many os and is enough for 99.9% situations.
-// So, we hope you will never use it :)
+// The another reason choosing to use it is .
+// So, better performance should not be the first reason to use it.
+// The first reason to use it is reducing gc objects, but we hope you never use it :)
 type Clock struct {
 	now int64
 }
@@ -62,9 +75,10 @@ func (c *Clock) start() {
 	for {
 		for i := 0; i < 9; i++ {
 			time.Sleep(duration)
-			atomic.AddInt64(&c.now, duration.Nanoseconds())
+			atomic.AddInt64(&c.now, int64(duration))
 		}
 
+		time.Sleep(100 * time.Millisecond)
 		atomic.StoreInt64(&c.now, time.Now().UnixNano())
 	}
 }
