86c89f40f11ecebda9639713921cdec7d1a80c59
FishGoddess <1149062639@qq.com>
2023-02-05 23:03:21 +0800

    增加 fast clock 实现

---
+++ pkg/clock/clock.go
@@ -0,0 +1,68 @@
+// Copyright 2023 FishGoddess. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package clock
+
+import (
+	"sync/atomic"
+	"time"
+)
+
+const (
+	duration = 100 * time.Millisecond
+)
+
+// Clock is a fast clock for getting current time.
+// It caches time and updates it in fixed duration which may return an "incorrect" time compared with time.Now().UnixNano().
+// In fact, we don't recommend you to use it unless you have to...
+// According to our benchmarks, it does run faster than time.Now:
+//
+// $ go test -bench=. -benchtime=1s ./clock.go ./clock_test.go
+// goos: windows
+// goarch: amd64
+// cpu: AMD Ryzen 7 5800X 8-Core Processor
+// BenchmarkTimeNow-16             338466458                3.523 ns/op           0 B/op          0 allocs/op
+// BenchmarkClockNow-16            1000000000               0.2165 ns/op          0 B/op          0 allocs/op
+//
+// However, the performance of time.Now is faster enough in many os and is enough for 99.9% situations.
+// So, we hope you will never use it :)
+type Clock struct {
+	now int64
+}
+
+// New creates a new clock which caches time and updates it in fixed duration.
+func New() *Clock {
+	clock := &Clock{
+		now: time.Now().UnixNano(),
+	}
+
+	go clock.start()
+	return clock
+}
+
+func (c *Clock) start() {
+	for {
+		for i := 0; i < 9; i++ {
+			time.Sleep(duration)
+			atomic.AddInt64(&c.now, duration.Nanoseconds())
+		}
+
+		atomic.StoreInt64(&c.now, time.Now().UnixNano())
+	}
+}
+
+// Now returns the current time in nanoseconds.
+func (c *Clock) Now() int64 {
+	return atomic.LoadInt64(&c.now)
+}
---
+++ pkg/clock/clock_test.go
@@ -0,0 +1,58 @@
+// Copyright 2023 FishGoddess. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package clock
+
+import (
+	"math"
+	"testing"
+	"time"
+)
+
+// go test -bench=^BenchmarkTimeNow$ -benchtime=1s ./clock.go ./clock_test.go
+func BenchmarkTimeNow(b *testing.B) {
+	b.ReportAllocs()
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		time.Now().Nanosecond()
+	}
+}
+
+// go test -bench=^BenchmarkClockNow$ -benchtime=1s ./clock.go ./clock_test.go
+func BenchmarkClockNow(b *testing.B) {
+	clock := New()
+
+	b.ReportAllocs()
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		clock.Now()
+	}
+}
+
+// go test -v -cover -run=^TestClock$
+func TestClock(t *testing.T) {
+	clock := New()
+
+	for i := 0; i < 10; i++ {
+		now := clock.Now()
+		t.Log(now)
+
+		expect := time.Now().UnixNano()
+		if math.Abs(float64(expect-now)) > float64(duration)*1.5 {
+			t.Errorf("now %d is wrong with expect %d", now, expect)
+		}
+	}
+}
