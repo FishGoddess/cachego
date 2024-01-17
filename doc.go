5a6e6e83ddf9e9f11a91fd82c949e6e1473ceaf5
FishGoddess <1149062639@qq.com>
2024-01-18 00:40:45 +0800

    调整代码

--- doc.go
+++ doc.go
@@ -475,4 +475,4 @@ Package cachego provides an easy way to use foundation for your caching operatio
 package cachego // import "github.com/FishGoddess/cachego"
 
 // Version is the version string representation of cachego.
-const Version = "v0.6.0-alpha"
+const Version = "v0.6.1"
